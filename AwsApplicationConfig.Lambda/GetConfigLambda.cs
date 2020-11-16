8a1d1bf79759f9d23963572e2bab975ae8d1fad2
Wojciech Dąbrowski <wojc.dabrowski@gmail.com>
2020-11-16 16:17:12 +0100

    Add optional getting config from parameter store

--- AwsApplicationConfig.Lambda/GetConfigLambda.cs
+++ AwsApplicationConfig.Lambda/GetConfigLambda.cs
@@ -7,7 +7,14 @@ namespace AwsApplicationConfig.Lambda
     public class GetConfigLambda
     {
         public async Task<ConfigModel> Invoke()
-        { 
+        {
+            // return GetConfigUsingParameterStore();
+
+            return await GetConfigUsingAppConfigAsync();
+        }
+
+        private async Task<ConfigModel> GetConfigUsingAppConfigAsync()
+        {
             var appConfigAppId = Environment.GetEnvironmentVariable("APP_CONFIG_APP_ID");
             var appConfigEnvironmentId = Environment.GetEnvironmentVariable("APP_CONFIG_ENVIRONMENT_ID");
             var appConfigConfigProfileId = Environment.GetEnvironmentVariable("APP_CONFIG_CONFIG_PROFILE_ID");
@@ -16,5 +23,7 @@ namespace AwsApplicationConfig.Lambda
             var appConfigReader = new AwsAppConfigReader(appConfigAppId, appConfigEnvironmentId, clientId, appConfigConfigProfileId);
             return await appConfigReader.GetConfig();
         }
+
+        private static ConfigModel GetConfigUsingParameterStore() => ConfigurationReader.GetConfig();
     }
 }
