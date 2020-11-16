daf68b2c5eef8b043bb9112c232749d84013e0ad
Wojciech Dąbrowski <wojc.dabrowski@gmail.com>
2020-11-16 22:24:27 +0100

    Add using for extension reader

--- AwsApplicationConfig.Lambda/Functions/GetConfigUsingExtensionLambda.cs
+++ AwsApplicationConfig.Lambda/Functions/GetConfigUsingExtensionLambda.cs
@@ -12,7 +12,7 @@ namespace AwsApplicationConfig.Lambda.Functions
             var appConfigConfig = AwsAppConfigConfigurationReader.GetConfiguration();
             int.TryParse(Environment.GetEnvironmentVariable("AWS_APPCONFIG_EXTENSION_HTTP_PORT"), out var port);
 
-            var configReader = new AwsAppConfigLambdaExtensionReader(appConfigConfig, port);
+            using var configReader = new AwsAppConfigLambdaExtensionReader(appConfigConfig, port);
 
             return await configReader.GetConfigAsync();
         }
